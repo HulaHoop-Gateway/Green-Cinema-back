135aad504a1f0faf631acd3d9a9f05a74e5919e7
smin0111 <seungmin6385@gmail.com>
2025-11-26 17:34:22 +0900

    [FEAT]예약 내역 그룹조회

--- src/main/java/com/novacinema/schedule/model/dao/ScheduleMapper.java
+++ src/main/java/com/novacinema/schedule/model/dao/ScheduleMapper.java
@@ -17,9 +17,11 @@ public interface ScheduleMapper {
     // 🔥 날짜 필터링 적용된 스케줄 조회 (오늘/내일/특정 날짜)
     List<ScheduleDTO> findSchedulesByBranchNumAndDate(
             @Param("branchNum") String branchNum,
-            @Param("screeningDate") String screeningDate
-    );
+            @Param("screeningDate") String screeningDate);
 
     // ⭐ scheduleNum 으로 merchant_code 조회 추가
     String findMerchantCodeByScheduleNum(@Param("scheduleNum") int scheduleNum);
+
+    // ⭐ scheduleNum 으로 상세 스케줄 조회
+    ScheduleDTO selectScheduleByNum(@Param("scheduleNum") int scheduleNum);
 }
